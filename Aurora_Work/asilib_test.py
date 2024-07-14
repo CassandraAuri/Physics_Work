cc2dfadbcaf5ca753cf1365e5d910b9fd323298e
Cassandra Mckenna <105663287+CassandraAuri@users.noreply.github.com>
2024-07-14 13:22:37 -0600

    Update asilib_test.py

--- Aurora_Work/asilib_test.py
+++ Aurora_Work/asilib_test.py
@@ -12,14 +12,14 @@ asilib.config["acknowledged_asis"] = []
 st.write(os.path.dirname(os.path.abspath(asilib.acknowledge.__file__)))
 st.write(asilib.config["acknowledged_asis"])
 
-time_range=[datetime(2021,4,16,6,39), datetime(2021,4,16,6,42)]
+time_range=[datetime(2021,4,16,6,39)]
 try:
     st.write(
           'start'
     )
     asi = asilib.asi.trex.trex_rgb(
         'fsmi',
-        time_range=time_range,
+        time=time_range,
         alt=110,
         colors="rgb",
         custom_alt=True
@@ -28,7 +28,7 @@ except PermissionError:
         st.write('erro1')
         asi = asilib.asi.trex.trex_rgb(
         'fsmi',
-        time_range=time_range,
+        time=time_range,
         alt=110,
         colors="rgb",
         custom_alt=True
