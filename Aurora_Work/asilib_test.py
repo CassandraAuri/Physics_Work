8c1569c5e45ccc834ee0de8532e773e1926a5f0f
Cassandra Mckenna <105663287+CassandraAuri@users.noreply.github.com>
2024-07-14 12:21:29 -0600

    Update asilib_test.py

--- Aurora_Work/asilib_test.py
+++ Aurora_Work/asilib_test.py
@@ -9,15 +9,10 @@ asilib.config["ASI_DATA_DIR"] = os.path.dirname(os.path.abspath(__file__)) #Chan
 asilib.config['ASILIB_DIR'] = os.path.dirname(os.path.abspath(__file__)) #Changes directory to the src of so writing can happen
 asilib.config["HERE"] = os.path.dirname(os.path.abspath(__file__))
 asilib.config["acknowledged_asis"] = []
-path = "/mount"
+print(asilib.config["acknowledged_asis"])
 
-# Walk through the directory and its subdirectories
-for root, dirs, files in os.walk(path):
-    st.write(f"Directory: {root}")
-    for file in files:
-        st.write(f" - {file}")
 time_range=[datetime(2021,4,16,6,39), datetime(2021,4,16,7,42)]
-"""
+
 asi = asilib.asi.trex.trex_rgb(
     'fsmi',
     time_range=time_range,
@@ -25,4 +20,4 @@ asi = asilib.asi.trex.trex_rgb(
     colors="rgb",
     custom_alt=True
 )
-"""
+
