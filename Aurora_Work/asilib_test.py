b637c77cf434fbf4a294c65dcc09c5c05166ad1b
Cassandra Mckenna <105663287+CassandraAuri@users.noreply.github.com>
2024-07-14 11:31:38 -0600

    Update asilib_test.py

--- Aurora_Work/asilib_test.py
+++ Aurora_Work/asilib_test.py
@@ -4,8 +4,9 @@ import asilib
 import asilib.asi
 from datetime import datetime
 import os
-asilib.config["ASI_DATA_DIR"] = os.path.dirname(os.path.abspath(__file__))
-asilib.config['ASILIB_DIR'] = os.path.dirname(os.path.abspath(__file__))
+asilib.config["ASI_DATA_DIR"] = os.path.dirname(os.path.abspath(__file__)) #Changes directory to the src of so writing can happen
+asilib.config['ASILIB_DIR'] = os.path.dirname(os.path.abspath(__file__)) #Changes directory to the src of so writing can happen
+asilib.config["HERE"] = os.path.dirname(os.path.abspath(__file__))
 time_range=[datetime(2021,4,16,6,39), datetime(2021,4,16,7,42)]
 asi = asilib.asi.trex.trex_rgb(
     'fsmi',
