6a4e3ab32117aebea24b48a41f384217a9fe691a
Cassandra Mckenna <cmckenna@ualberta.ca>
2024-07-13 23:09:49 -0600

    Update home.py

--- Aurora_Work/home.py
+++ Aurora_Work/home.py
@@ -56,6 +56,7 @@ st.write("REGO : Old camera, Focuses on Nitrogen Emissions  \
 st.write("TREx-NIR: New camera, focuses on Near Infared, altitude should always be 150km, lower resolution than other cameras and longer integration time: 1 image every 6 seconds")
 st.write("TREx-RGB:  New camera, RECOMMENDED,  focuses on Optical Emission, altitude be 110km unless otherwise specified, works very well, 1 image every 3 seconds")
 st.write(asilib.config["ASI_DATA_DIR"])
+st.write(asilib.config)
 import os
  
 st.write('Absolute path of file:     ', 
