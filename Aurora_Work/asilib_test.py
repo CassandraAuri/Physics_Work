79cb0335f2587f8cf39b212ca70454fde433dbfc
Cassandra Mckenna <105663287+CassandraAuri@users.noreply.github.com>
2024-07-14 11:19:23 -0600

    Update asilib_test.py

--- Aurora_Work/asilib_test.py
+++ Aurora_Work/asilib_test.py
@@ -3,6 +3,9 @@ import streamlit as st
 import asilib
 import asilib.asi
 from datetime import datetime
+import os
+asilib.config["ASI_DATA_DIR"] = os.path.dirname(os.path.abspath(__file__))
+asilib.config['ASILIB_DIR'] = os.path.dirname(os.path.abspath(__file__))
 time_range=[datetime(2021,4,16,6,39), datetime(2021,4,16,7,42)]
 asi = asilib.asi.trex.trex_rgb(
     'fsmi',
