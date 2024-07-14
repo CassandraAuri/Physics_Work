1752c39116b192bc430ae75e2343ac12fe205ff3
Cassandra Mckenna <105663287+CassandraAuri@users.noreply.github.com>
2024-07-14 13:30:40 -0600

    Update asilib_test.py

--- Aurora_Work/asilib_test.py
+++ Aurora_Work/asilib_test.py
@@ -8,9 +8,7 @@ st.write(os.path.dirname(os.path.abspath(__file__)))
 asilib.config["ASI_DATA_DIR"] = os.path.dirname(os.path.abspath(__file__)) #Changes directory to the src of so writing can happen
 asilib.config['ASILIB_DIR'] = os.path.dirname(os.path.abspath(__file__)) #Changes directory to the src of so writing can happen
 asilib.config["HERE"] = os.path.dirname(os.path.abspath(__file__))
-asilib.config["acknowledged_asis"] = []
-st.write(os.path.dirname(os.path.abspath(asilib.acknowledge.__file__)))
-st.write(asilib.config["acknowledged_asis"])
+
 
 time_range=datetime(2021,4,16,6,39, 0)
 try:
