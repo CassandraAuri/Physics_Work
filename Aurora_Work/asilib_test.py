aee94c66c21d24fc0834c7d485b5dad47bdd037b
Cassandra Mckenna <105663287+CassandraAuri@users.noreply.github.com>
2024-07-14 12:01:54 -0600

    Update asilib_test.py

--- Aurora_Work/asilib_test.py
+++ Aurora_Work/asilib_test.py
@@ -9,18 +9,13 @@ asilib.config["ASI_DATA_DIR"] = os.path.dirname(os.path.abspath(__file__)) #Chan
 asilib.config['ASILIB_DIR'] = os.path.dirname(os.path.abspath(__file__)) #Changes directory to the src of so writing can happen
 asilib.config["HERE"] = os.path.dirname(os.path.abspath(__file__))
 asilib.config["acknowledged_asis"] = []
-def print_directory_contents(path):
-    for root, dirs, files in os.walk(path):
-        level = root.replace(path, '').count(os.sep)
-        indent = ' ' * 4 * level
-        st.write(f'{indent}{os.path.basename(root)}/')
-        sub_indent = ' ' * 4 * (level + 1)
-        for f in files:
-            st.write(f'{sub_indent}{f}')
+path = "/mount"
 
-# Replace 'your_directory_path' with the path of the directory you want to print
-print_directory_contents(r'mount/src')
-
+# Walk through the directory and its subdirectories
+for root, dirs, files in os.walk(path):
+    print(f"Directory: {root}")
+    for file in files:
+        print(f" - {file}")
 time_range=[datetime(2021,4,16,6,39), datetime(2021,4,16,7,42)]
 """
 asi = asilib.asi.trex.trex_rgb(
