1cc94d5515bad89bdb9b3471dda9dacf7e569538
Cassandra Mckenna <cmckenna@ualberta.ca>
2025-02-04 08:33:40 -0700

    Update fig1animation.py

--- Aurora_Work/Figures_paper/fig1animation.py
+++ Aurora_Work/Figures_paper/fig1animation.py
@@ -15,7 +15,7 @@ import scienceplots
 
 plt.style.use(['science','no-latex'])
 alt=150
-time_array=(datetime(2022,12,19,14,2), datetime(2022,12,19,14,8))
+time_array=(datetime(2022,12,19,14,4), datetime(2022,12,19,14,8))
 
 #From documentation link
 def requester(sc_collection, measurement, residual, sampling_step=None, **kwargs):
@@ -225,15 +225,16 @@ V_perpendicular = VNEC[:, 1] / np.max(np.abs(VNEC[:, 1]))   # Keeping the x-comp
 
 
 asi=asilib.asi.trex_rgb(location_code='yknf', alt=alt, time_range=time_array, colors='rgb')
-fig,ax =plt.subplots(figsize=(5.11,4.04))
-movie_generator = asi.animate_map_gen(asi_label=False, ax=ax)
+#fig,ax =plt.subplots(figsize=(8,4.5), dpi=300, tight_layout=True)
+fig,ax =plt.subplots(figsize=(6,4.5), dpi=300, tight_layout=True)
+movie_generator = asi.animate_map_gen(asi_label=False, ax=ax, ffmpeg_params={"framerate": 5})
 quivers = None  # Initialize quivers as None
 quivers_red = None  # Initialize quivers_red as None
 for i, (time, image, axes, _) in enumerate(movie_generator):
+    print(type(time))
+    plt.title((time-timedelta(hours=7)).strftime("%H:%M:%S")+ " Local Time")
 
-    plt.title(time)
-
-    ax.set_xlim(-119,-111)
+    ax.set_xlim(-119,-110)
     ax.set_ylim(61.5,63.4)
 
     if quivers is not None:
@@ -241,7 +242,14 @@ for i, (time, image, axes, _) in enumerate(movie_generator):
     if quivers_red is not None:
         quivers_red.remove()
 
-    quivers=ax.quiver( sat_lla_E[1], sat_lla_E[0], V_perpendicular, u_zeros, color='white', scale=1, scale_units='inches', alpha=0.15)
+    #quivers=ax.quiver( sat_lla_E[1], sat_lla_E[0], V_perpendicular, u_zeros, color='white', scale=1, scale_units='inches', alpha=0.15)
     indicies_sat= (np.where((Etime >= np.datetime64(time)) & (Etime <= np.datetime64(time + timedelta(seconds=3))))[0])
-    quivers_red=ax.quiver( sat_lla_E[1][indicies_sat], sat_lla_E[0][indicies_sat], V_perpendicular[indicies_sat], u_zeros[indicies_sat], color='red', scale=1, scale_units='inches', alpha=0.25)
-    print(sat_lla_E[0][indicies_sat], sat_lla_E[1][indicies_sat])
+    if indicies_sat.size == 0:
+        pass
+    else:
+        print('yipee', sat_lla_E[1][indicies_sat[0]], sat_lla_E[1][indicies_sat[0]],)
+        ax.scatter(sat_lla_E[1][indicies_sat[0]], sat_lla_E[0][indicies_sat[0]], color='red', s=25)
+    ax.plot(sat_lla_E[1], sat_lla_E[0], linestyle='--', dashes=(5, 20), color='white') #length of 5, space of 20  
+    #quivers_red=ax.quiver( sat_lla_E[1][indicies_sat], sat_lla_E[0][indicies_sat], V_perpendicular[indicies_sat], u_zeros[indicies_sat], color='red', scale=1.75, scale_units='inches', alpha=0.25)
+    ax.set_ylabel("Geographic Latitude")
+    ax.set_xlabel("Geographic Longitude")
