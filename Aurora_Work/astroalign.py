513e21732d7afe5679eb809a5da3d3470673168f
Cassandra Mckenna <cmckenna@ualberta.ca>
2025-06-01 12:17:11 -0600

    Removed unneeded files

--- Aurora_Work/astroalign.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import astroalign as aa
-source = Image.open(r"D:\space_photos2024\pk111_Ha\haoiimaybe.tif")
-target = Image.open(r"D:\space_photos2024\pk111_Ha\rgbdone.tif")
-registered, footprint = aa.register(source, target)
-# Convert back to pillow image if necessary:
-registered = Image.fromarray(registered.astype("unit8"))
